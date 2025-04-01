3c368f219d6a247b19ec743eb23a2570e9d362d7
google06191 <93990963+google06191@users.noreply.github.com>
2025-04-01 13:30:20 +0700

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request,render_template
-from Model import SpellCheckerModule
+from model3 import SpellCheckerModule
 
 app = Flask(__name__)
 spell_checker_module = SpellCheckerModule()
